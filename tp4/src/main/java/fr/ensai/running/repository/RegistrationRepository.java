e9c854d45d335808da7e043063c5d79f9da44542
guenolee <rayane.djidjelli@eleve.ensai.fr>
2025-04-07 10:33:52 +0000

    Correctif pour le lancement de l'application

--- tp4/src/main/java/fr/ensai/running/repository/RegistrationRepository.java
+++ tp4/src/main/java/fr/ensai/running/repository/RegistrationRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.ensai.running.model.Athlete;
 import fr.ensai.running.model.Competition;
@@ -13,6 +14,7 @@ public interface RegistrationRepository extends JpaRepository<Registration, Long
 
     Registration findByAthleteAndCompetition(Athlete athlete, Competition competition);
 
-    @Query("SELECT r.athlete.idAthlete FROM Registration r WHERE r.competition.idCompetition = :idCompetition")
-    List<Long> findAthleteIdByCompetitionId(Long idCompetition);
+    @Query("SELECT r.athlete.id FROM Registration r WHERE r.competition.id = :idCompetition")
+    List<Long> findAthleteIdByCompetitionId(@Param("idCompetition") Long idCompetition);
+
 }
